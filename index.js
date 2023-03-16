9a99b0106cd30a8ec849d2fe6c3bb2daa019a73f
changach-3000 <sharon.changach@student.moringaschool.com>
2023-03-16 06:42:13 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value){
+   let dest = 42;
+   let diff;
+   diff = Math.abs(value - dest);
+   return diff;
+ }
+
+ function distanceFromHqInFeet(value){
+   distanceFromHqInBlocks(value);
+   const convert = 264;
+   return distanceFromHqInBlocks(value) * convert;
+
+}
+function distanceTravelledInFeet(start, dest){
+   const convert = 264 ;
+   const distance = Math.abs(dest-start);
+   return  distance * convert;
+
+}
+
+function calculatesFarePrice(start, dest){
+   distanceTravelledInFeet(start, dest);
+   let price;
+   let set = 25;
+   if (distanceTravelledInFeet(start,dest)<= 400){
+       return price = 0;
+   }else if (distanceTravelledInFeet(start,dest) > 400 && distanceTravelledInFeet(start,dest) <= 2000){
+      return price = (2/100 * (distanceTravelledInFeet(start,dest)-400));
+   }else if(distanceTravelledInFeet(start,dest) > 2000 && distanceTravelledInFeet(start,dest)<2500){
+      return set;
+
+   }else{
+      return "cannot travel that far";
+   }
+
+}
+
+ 
+
